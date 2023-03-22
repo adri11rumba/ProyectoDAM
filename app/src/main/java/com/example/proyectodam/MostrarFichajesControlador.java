a6ab02011a71aafebc900e82bfd6556c73612cb3
adri11rumba <sobrasada32@gmail.com>
2023-03-22 15:48:23 +0100

    Dejo la busqueda sin limite y orden automatico

--- app/src/main/java/com/example/proyectodam/MostrarFichajesControlador.java
+++ app/src/main/java/com/example/proyectodam/MostrarFichajesControlador.java
@@ -26,7 +26,7 @@ public class MostrarFichajesControlador extends AppCompatActivity {
         bd = FirebaseFirestore.getInstance();
         String DNI = getIntent().getExtras().getString("DNI_recogido");
 
-        Query query = bd.collection("REGISTROS DE LOS EMPLEADOS").limit(15).whereEqualTo("DNI", DNI);
+        Query query = bd.collection("REGISTROS DE LOS EMPLEADOS").whereEqualTo("DNI", DNI);
 
         FirestoreRecyclerOptions<Empleado> firestoreRecyclerOptions = new FirestoreRecyclerOptions.Builder<Empleado>().setQuery(query,Empleado.class).build();
 
